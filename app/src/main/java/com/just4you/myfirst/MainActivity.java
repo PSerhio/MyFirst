b14cdf8235716c1a33aa8d519805b85f03a736a8
PSerhio <5075720@mail.ru>
2015-10-19 18:48:16 +0300

    Первичное размещение кода

--- app/src/main/java/com/just4you/myfirst/MainActivity.java
+++ app/src/main/java/com/just4you/myfirst/MainActivity.java
@@ -1,26 +1,28 @@
 package com.just4you.myfirst;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 //
-public class MainActivity extends ListActivity {
-    String[] country = {"Россия","Белоруссия","Германия","Польша","Бельгия","Нидерланды",
-                        "Куба","США","Турция","Египет","Сирия","Китай","Южная Корея","Украина",
-                        "Таиланд","Латвия","Кипр"};
+public class MainActivity extends Activity {
+    String[] country = {"Россия", "Белоруссия", "Германия", "Польша", "Бельгия", "Нидерланды",
+            "Куба", "США", "Турция", "Египет", "Сирия", "Китай", "Южная Корея", "Украина",
+            "Таиланд", "Латвия", "Кипр"};
     Integer[] FlagArray = {R.drawable.russia, R.drawable.belarus_, R.drawable.germany,
-             R.drawable.poland, R.drawable.belgium,R.drawable.netherlands, R.drawable.cuba,
-                R.drawable.usa, R.drawable.turkey, R.drawable.egypt,R.drawable.syria,
-                R.drawable.china, R.drawable.south_korea, R.drawable.ukraine, R.drawable.thailand,
-                R.drawable.latvia, R.drawable.cyprus};
+            R.drawable.poland, R.drawable.belgium, R.drawable.netherlands, R.drawable.cuba,
+            R.drawable.usa, R.drawable.turkey, R.drawable.egypt, R.drawable.syria,
+            R.drawable.china, R.drawable.south_korea, R.drawable.ukraine, R.drawable.thailand,
+            R.drawable.latvia, R.drawable.cyprus};
 
     private MyAdapter mAdapter;
 
@@ -28,16 +30,24 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAdapter = new MyAdapter(this);
-        setListAdapter(mAdapter);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(mAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Номер в списке " + (position + 1), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Toast.makeText(getApplicationContext(),
-                "Номер в списке " + (position + 1), Toast.LENGTH_SHORT).show();
-    }
+
+
     public class MyAdapter extends BaseAdapter {
         private LayoutInflater mLayoutInflater;
+
         public MyAdapter(Context context) {
             mLayoutInflater = LayoutInflater.from(context);
         }
@@ -46,18 +56,20 @@ public class MainActivity extends ListActivity {
         public int getCount() {
             return country.length;
         }
+
         @Override
         public Object getItem(int position) {
             return position;
         }
+
         @Override
         public long getItemId(int position) {
             return position;
         }
 
-        public View getView(int position, View convertView, ViewGroup parent){
+        public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null)
-               convertView = mLayoutInflater.inflate(R.layout.item, null);
+                convertView = mLayoutInflater.inflate(R.layout.item, null);
             ImageView image = (ImageView) convertView.findViewById(R.id.ivImg);
             image.setImageResource(FlagArray[position]);
             TextView countryTextView = (TextView) convertView.findViewById(R.id.tvText);
